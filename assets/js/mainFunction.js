bb9e1954db5aff602ac6ac422a9241b299c0295c
destroylord <masapin68@gmail.com>
2021-11-02 21:04:01 +0700

    feature: not fixed yet landing pages

---
+++ assets/js/mainFunction.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
+
+// $(function() {
+//     var includes = $('[data-include]')
+//     var url = "file:///D:/laragon/www/mebel-landing-pages";
+//     $.each(includes, function() {
+//         var file = url+'/vendor/common/' + $(this).data('include') + '.html';
+//         $(this).load(file)
+//     })
+// })
